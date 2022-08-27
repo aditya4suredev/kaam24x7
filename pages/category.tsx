b63e75ee10791fbdb61fb98abfa8527c25888368
aditya4suredev <aditya4suredev@gmail.com>
2022-08-27 19:04:49 +0530

    first commit2

--- pages/category.tsx
+++ pages/category.tsx
@@ -36,7 +36,7 @@ function category({ categories, products }: Props) {
       
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <span className="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-            <img src="/sofa.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+            <img src="/sofa.png" className="absolute object-contain block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
 
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
