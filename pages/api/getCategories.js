95f1a225a7b0798d9c290b23d56ec3ef341e3114
aditya4suredev <112243423+aditya4suredev@users.noreply.github.com>
2023-05-29 20:14:54 +0530

    Update getCategories.js

--- pages/api/getCategories.js
+++ pages/api/getCategories.js
@@ -3,12 +3,19 @@ import Category from '../../models/Category';
 
 connectDB();
 
-
 export default async function handler(req, res) {
-      try {
-        const categories = await Category.find();
-        res.status(200).json({ success: true, data: categories });
-      } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
-      }
-    }
+  try {
+    const categories = await Category.find();
+    const formattedCategories = categories.map((category) => ({
+      _id: category._id,
+      name: category.name,
+      image: category.image,
+      // Add any other category properties you need
+    }));
+
+    res.status(200).json({ success: true, data: formattedCategories });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+}
+
