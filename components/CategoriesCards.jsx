201055b6e2f8a145da547459c31ea8a5f55f6bda
aditya4suredev <112243423+aditya4suredev@users.noreply.github.com>
2023-05-29 20:16:37 +0530

    Update CategoriesCards.jsx

--- components/CategoriesCards.jsx
+++ components/CategoriesCards.jsx
@@ -6,34 +6,31 @@ const CategoriesCards = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-  const fetchCategories = async () => {
-    try {
-      const response = await axios.get('/api/getCategories');
-      setCategories(response.data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('/api/getCategories');
+        setCategories(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  fetchCategories();
-}, []);
-
+    fetchCategories();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-     
         {categories.map((category, index) => (
-          <Link href={`/subcategory/${category._id}`}>
+          <Link key={category._id} href={`/category/${category._id}`}>
             <a className={`relative rounded-lg overflow-hidden cursor-pointer hover:text-white hover:bg-[#3FE0C5] ${index < 5 ? 'bg-[#F6F4EE]' : 'bg-gray-200'}`}>
               <img src={category.image} alt={category.name} className="object-cover h-48 w-full" />
               <div className="p-4">
                 <h2 className="text-2xl font-bold text-center font-darkage mb-2">{category.name}</h2>
               </div>
             </a>
-            </Link>
+          </Link>
         ))}
-    
       </div>
     </div>
   );
